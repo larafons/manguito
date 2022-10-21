c31eb6c837b5c48d43b7821d91afc90e71912c57
Lara Fons <larafons94@gmail.com>
2022-10-21 17:01:29 -0300

    comentario

--- Manguito/src/main/java/Emprendedor.java
+++ Manguito/src/main/java/Emprendedor.java
@@ -3,6 +3,30 @@ import javax.persistence.*;
 @Entity
 public class Emprendedor {
 	
+	public Long getId() {
+		return id;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public Emprendimiento getEmprendimiento() {
+		return emprendimiento;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public void setEmprendimiento(Emprendimiento emprendimiento) {
+		this.emprendimiento = emprendimiento;
+	}
 	@Id @GeneratedValue
 	@Column(name="administrador_id")
 	private Long id;
@@ -10,7 +34,7 @@ public class Emprendedor {
 	private String passwd;
 	@Column
 	private String usuario;
-	//?????????????
+	@OneToOne
 	private Emprendimiento emprendimiento;
 		
 
